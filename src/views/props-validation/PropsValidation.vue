a682ba0fbcaca283b37c09985224d0eb496507dc
Viet Tran <tranxuanviet1997@gmail.com>
2025-03-05 15:58:44 +0700

    Props validation

---
+++ src/views/props-validation/PropsValidation.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps({
+  type: {
+    type: String,
+    validator(value: string): boolean {
+      return ['primary', 'ghost', 'dashed', 'link', 'text', 'default'].includes(value)
+    },
+    default: 'default',
+  },
+})
+</script>
+
+<template>
+  <button>Button</button>
+</template>
---
+++ src/views/props-validation/PropsValidationParent.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import Button from './PropsValidation.vue'
+</script>
+
+<template>
+  <Button />
+</template>
